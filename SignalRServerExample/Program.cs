c8c66db9a84a91e6f0d91e92485000bf46171616
CEMAL YILDIRIM <bystrong@technovadi.com>
2022-09-04 21:11:53 +0300

    Run Time Application Development with SignalR 1 - Developing Server Application

--- SignalRServerExample/Program.cs
+++ SignalRServerExample/Program.cs
@@ -1,6 +1,29 @@
+using SignalRServerExample.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.SetIsOriginAllowed(origin => true)
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+});
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<MyHub>("/myhub");
+});
 
 app.Run();
