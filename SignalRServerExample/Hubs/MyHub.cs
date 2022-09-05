1c9562d833f1cae3c92071a9a97f215fde67df32
CEMAL YILDIRIM <bystrong@technovadi.com>
2022-09-05 13:52:48 +0300

    Run Time Application Development with SignalR 3 - Connection Events | Listing All Clients

--- SignalRServerExample/Hubs/MyHub.cs
+++ SignalRServerExample/Hubs/MyHub.cs
@@ -4,9 +4,25 @@ namespace SignalRServerExample.Hubs
 {
     public class MyHub : Hub
     {
+        private static readonly List<string> clients = new List<string>();
+
         public async Task SendMessageAsync(string message)
         {
             await Clients.All.SendAsync("receiveMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            clients.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.Others.SendAsync("userJoined", Context.ConnectionId);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            clients.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.Others.SendAsync("userLeaved", Context.ConnectionId);
         }
     }
 }
